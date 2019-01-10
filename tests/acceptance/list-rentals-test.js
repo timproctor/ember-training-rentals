e39c5312d644f62737d4582704ddd739ad664a3d
timproctor <timandcamiproctor@gmail.com>
2019-01-09 21:38:10 -0500

    Create a search filter by city using controller.

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -1,5 +1,5 @@
 import { module, test } from 'qunit';
-import { click, currentURL, visit } from '@ember/test-helpers';
+import { click, currentURL, visit, fillIn, triggerKeyEvent } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 
@@ -11,23 +11,32 @@ module('Acceptance | list rentals', function(hooks) {
     await visit('/');
     assert.equal(currentURL(), '/rentals', 'should redirect automatically');
   });
+
   test('should link to information about the company', async function(assert) {
     await visit('/');
     await click(".menu-about");
     assert.equal(currentURL(), '/about', 'should navigate to about')
   });
+
   test('should link to contact information', async function(assert) {
     await visit('/');
     await click(".menu-contact");
     assert.equal(currentURL(), '/contact', 'should navigate to contact');
   });
+
   test('should list available rentals', async function(assert) {
     await visit('/');
     assert.equal(this.element.querySelectorAll('.listing').length, 3, 'should display 3 listings');
   });
-  test('should filter the list of rentals by city', async function(assert) {
 
+  test('should filter the list of rentals by city', async function(assert) {
+    await visit('/');
+    await fillIn('.list-filter input', 'seattle');
+    await triggerKeyEvent('.list-filter input', 'keyup', 69);
+    assert.equal(this.element.querySelectorAll('.results .listings').length, 1, 'should display 1 listing');
+    assert.ok(this.element.querySelector('.listing .location').textContent.includes('Seattle'), 'should contain 1 listing with location Seattle');
   });
+
   test('should show details for a selected rental', async function (assert) {
   });
 });
