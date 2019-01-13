222ee54729b8cce8333d67561601839a0e8a5033
timproctor <timandcamiproctor@gmail.com>
2019-01-12 20:54:35 -0500

    Connect a /show route and pass all specs

--- tests/integration/components/rental-listing-test.js
+++ tests/integration/components/rental-listing-test.js
@@ -2,21 +2,28 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render, click } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
-import EmberObject from '@ember/object';
+import Service from '@ember/service';
+import { resolve } from 'rsvp';
 
+let StubMapsService = Service.extend({
+  getMapElement() {
+    return resolve(document.createElement('div'));
+  }
+});
 
-module('Integration | Component | rental listing', function (hooks) {
+module('Integration | Component | rental listing', function(hooks) {
   setupRenderingTest(hooks);
 
   hooks.beforeEach(function () {
-    this.rental = EmberObject.create({
+    this.owner.register('service:map-element', StubMapsService);
+    this.rental = {
       image: 'fake.png',
       title: 'test-title',
       owner: 'test-owner',
       type: 'test-type',
       city: 'test-city',
       bedrooms: 3
-    });
+    };
   });
 
   test('should display rental details', async function(assert) {
